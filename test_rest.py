5d2dd629ae0f5d6989b79783675e218ebb9c8ac8
len_w <len_wanger@hotmail.com>
2025-11-20 23:22:15 -0600

    Some cleanup of methods

--- test_rest.py
+++ test_rest.py
@@ -1,21 +1,11 @@
 """
 Test Insperity REST API
 
-URL: https://myisolved.com/rest
-
-Note that your production calls will be to insperity.myisolved.com
-legal id 2502007-1,2,3 (ves, npt, lv)
-
-Here is the information you need for immediate access to the API Specifications:
-
-URL: https://myisolved.com/rest
-
 Sample endpoints on the call:
 
-To get your client ID and legal company ID that will be referenced in other endpoints:
-List of Clients - https://insperity.myisolved.com/rest/api/clients
-List of Legal Companies - https://insperity.myisolved.com/rest/api/legals
-
+    To get your client ID and legal company ID that will be referenced in other endpoints:
+    List of Clients - https://insperity.myisolved.com/rest/api/clients
+    List of Legal Companies - https://insperity.myisolved.com/rest/api/legals
 
 To get the employee IDs that will be referenced in the other endpoints:
 Get Minimal Employee List – returns the employee ID, name, email, and a few other basic employee details - https://insperity.myisolved.com/rest/api/clients/{clientId}/legals/{legalId}/employeesMinimal
@@ -29,8 +19,11 @@ Alerts in the timecardVerifications dataset can include an approaching overtime 
 Employee Dashboard - https://insperity.myisolved.com/rest/api/clients/{clientId}/legals/{legalId}/dashboard/filterOptions
 
 TODO:
-    - implement request of access token
+    - get client and legal ids for other calls
+    - implement request of refresh token
     - handle paginated results
+    - move constants to another file
+    - safety: check return statuses and do error checking!
 """
 
 import base64
@@ -45,7 +38,6 @@ LEGAL_ID_NPT = '2502007-2'
 LEGAL_ID_LV = '2502007-3'
 
 BASE_URL = "https://insperity.myisolved.com/rest/api"
-# GET_TOKEN = "https://insperity.myisolved.com/rest/api/token"  # POST
 GET_TOKEN = f"{BASE_URL}/token"  # POST
 CLIENTS = f"{BASE_URL}/clients"  # "https://insperity.myisolved.com/rest/api/clients"
 LEGALS = f"{BASE_URL}/legals"  # "https://insperity.myisolved.com/rest/api/legals"
@@ -56,23 +48,19 @@ def to_mime_base64(input_string: str) -> str:
     """
     Convert a string to RFC2045-MIME variant of Base64.
     """
-    # Encode string to bytes
     byte_data = input_string.encode("utf-8")
     mime_encoded = base64.b64encode(byte_data)
     return mime_encoded.decode("utf-8")
 
-def get_combined_key() -> str:
-    # client_id = os.getenv('INSPERITY_CLIENT_ID')
-    # secret = os.getenv('INSPERITY_SECRET')
-    # user = os.getenv('INSPERITY_USER')
-    # pwd = os.getenv('INSPERITY_PWD')
-    combined_key = to_mime_base64(f"{os.getenv('INSPERITY_CLIENT_ID')}:{os.getenv('INSPERITY_SECRET')}")
 
-    return combined_key
+def get_combined_key() -> str:
+    return to_mime_base64(f"{os.getenv('INSPERITY_CLIENT_ID')}:{os.getenv('INSPERITY_SECRET')}")
 
 
 def get_client_password_token() -> str:
-    # MFA is not working.... URL can't be found
+    """
+    This function is NOT working! MFA is not working.... URL can't be found
+    """
     # get access token
     combined_key = get_combined_key()
 
@@ -96,27 +84,27 @@ def get_client_password_token() -> str:
     payload['mfa_token'] = mfa_token
 
     # get auth code from SMS
-    idx = response_dict['mfaSMSRoute'].find('?')
-    queries = response_dict['mfaSMSRoute'][idx+1:].split('&')
+    route_type = "mfaSMSRoute"
+    # route_type = "mfaEmailRoute"
 
-    # idx = response_dict['mfaEmailRoute'].find('?')
-    # queries = response_dict['mfaEmailRoute'][idx + 1:].split('&')
+    idx = response_dict[route_type].find('?')
+    queries = response_dict[route_type][idx+1:].split('&')
 
-    headers2 = {}
+    headers = {}
     for query in queries:
         if len(query) > 0:
             key, value = query.split('=')
-            headers2[key] = value
+            headers[key] = value
 
-    response = requests.post(url=response_dict['mfaSMSRoute'][:idx], headers=headers)
-    # response = requests.post(url=response_dict['mfaEmailRoute'][:idx], headers=headers2)
+    response = requests.post(url=response_dict[route_type][:idx], headers=headers)
+    print(f"{route_type=} {response.status_code=}: {response.text=}")
 
     mfa_auth_code = input("Enter MFA code: ")
     payload['mfa_code'] = mfa_auth_code
 
     response = requests.post(GET_TOKEN, headers=headers, data=payload)
 
-    print(f"{response.status_code=}: {response.text=}")
+    print(f"{route_type=} {response.status_code=}: {response.text=}")
     return response_dict['access_token']
 
 
@@ -148,8 +136,7 @@ def get_client_info(access_token: str) -> dict:
     # test getting client information
     response = requests.get(CLIENTS, headers=headers)
     response_dict = json.loads(response.content)
-    results = response_dict['results']
-    return results
+    return response_dict['results']
 
 
 if __name__ == '__main__':
